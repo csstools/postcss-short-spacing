483593247c6a9d099069569277d11c5e41df9e07
Fez Vrasta <federico.zivolo@gmail.com>
2016-06-10 16:28:11 +0200

    allow custom spacer instead of *
    fixed prev commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-short-spacing', function (opts) {
+	var spacer = opts && opts.spacer ? opts.spacer : '*';
 	var prefix = opts && opts.prefix ? '-' + opts.prefix + '-' : '';
 
 	return function (css) {
@@ -8,15 +9,15 @@ module.exports = postcss.plugin('postcss-short-spacing', function (opts) {
 			var name = prefix ? decl.prop.slice(prefix.length) : decl.prop;
 			var edge = postcss.list.space(decl.value);
 
-			if (edge.length && edge.indexOf('*') !== -1) {
+			if (edge.length && edge.indexOf(spacer) !== -1) {
 				if (!edge[1]) edge[1] = edge[0];
 				if (!edge[2]) edge[2] = edge[0];
 				if (!edge[3]) edge[3] = edge[1];
 
-				if (edge[0] !== '*') decl.cloneBefore({ prop: name + '-top',    value: edge[0] });
-				if (edge[1] !== '*') decl.cloneBefore({ prop: name + '-right',  value: edge[1] });
-				if (edge[2] !== '*') decl.cloneBefore({ prop: name + '-bottom', value: edge[2] });
-				if (edge[3] !== '*') decl.cloneBefore({ prop: name + '-left',   value: edge[3] });
+				if (edge[0] !== spacer) decl.cloneBefore({ prop: name + '-top',    value: edge[0] });
+				if (edge[1] !== spacer) decl.cloneBefore({ prop: name + '-right',  value: edge[1] });
+				if (edge[2] !== spacer) decl.cloneBefore({ prop: name + '-bottom', value: edge[2] });
+				if (edge[3] !== spacer) decl.cloneBefore({ prop: name + '-left',   value: edge[3] });
 
 				decl.remove();
 			} else if (prefix) decl.prop = name;
